260b939e9fc38836831e2e4d84259bfd89e2c711
Shivaarya <Shivaprasadadkhar27@gmail.com>
2025-04-20 23:20:01 +0530

    js

--- addNum.js
+++ addNum.js
@@ -1,6 +1,13 @@
+// function addNum(a,b){
+//     let c=a+b;
+//     console.log(c)
+// }
+ 
+// addNum(1,2);
+
 function addNum(a,b){
     let c=a+b;
     console.log(c)
 }
  
-addNum(1,2);
+addNum(1,2); 
